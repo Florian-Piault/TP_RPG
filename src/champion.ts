c4d80122906bba1c390aa9e17de048c3169444d1
Laaurent <laurent-quenel@hotmail.Fr>
2021-12-03 13:09:06 +0100

    implement protect

--- src/champion.ts
+++ src/champion.ts
@@ -1,26 +1,43 @@
+import { Knight } from "./knight";
+
 interface IChampion {
   protect(): void;
   attack(target: Champion): void;
   getHit(amount: number): void;
+  canDo(): Boolean;
 }
 
 export abstract class Champion implements IChampion {
   constructor(
     protected firstName: string,
     protected lastName: string,
+    protected numberOfTotalActions: number,
     protected numberOfActions: number,
     protected atk: number,
     protected def: number,
-    protected hp: number
+    protected hp: number,
+    protected isProtected: Boolean
   ) {}
 
-  public protect(): void {}
+  public protect(): void {
+    this.isProtected = true;
+  }
 
   public attack(target: Champion): void {
-    target.getHit(this.atk);
+    if (target.isProtected) {
+      if(!(target instanceof Knight))
+        target.getHit(this.atk / 2);
+    }
+    else {
+      target.getHit(this.atk);
+    }
   }
 
   public getHit(amount: number): void {
     this.hp -= amount;
+  }
+  
+  canDo(): Boolean{
+    return this.numberOfActions == 0 ? false : true
   }
 }
