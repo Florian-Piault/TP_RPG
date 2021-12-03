afb066c661e16e631f2165126a670f12ef805037
Florian PIAULT <florian.piault@mail-ecv.fr>
2021-12-03 13:33:21 +0100

    boss getTarget

--- src/boss.ts
+++ src/boss.ts
@@ -11,9 +11,14 @@ export class Boss extends Champion {
     super(firstName, lastName, 3, atk, def, hp);
   }
 
-  public attack(targets: Array<Champion>): void {
-    // attack a random target;
+  public getTarget(targets: Champion[]): Champion {
+    // get a random target if they're alive
     const length: number = targets.length;
-    const randomValue: number = Math.floor(Math.random() * length);
+    const randomIndex: number = Math.floor(Math.random() * length);
+    if (targets[randomIndex].isAlive()) return targets[randomIndex];
+
+    // if the target is dead, call the methods again
+    targets.splice(randomIndex, 1);
+    return this.getTarget(targets);
   }
 }
