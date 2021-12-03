e9bd9e6b1071f7741c894168bbac6472690e4a36
Soliman <Soliman.B15@gmail.com>
2021-12-03 16:23:44 +0100

    new heal PIXEL

--- src/mage.ts
+++ src/mage.ts
@@ -1,6 +1,7 @@
 import { Champion } from "./champion";
 import { Knight } from "./knight";
 import { Archer } from "./archer";
+const prompts = require('prompts');
 
 export class Mage extends Champion {
   constructor(
@@ -19,12 +20,14 @@ export class Mage extends Champion {
   public heal(targets: Champion[]): void {
     let power = 3
     let target: Champion
+    let aliveInput: number
+    let input: number
 
-    console.log('Qui voulez-vous heal ?')
     let alive = targets.filter((champion)=>{
-      champion.isAlive()
+      return champion.isAlive()
     })
     console.log('Voici les champions encore en vie !')
+    // console.log(alive)
     alive.forEach((champion, index)=>{
       if (champion instanceof Knight) {
         console.log(index+' - '+champion.getName()+' le chevalier')
@@ -32,27 +35,38 @@ export class Mage extends Champion {
         console.log(index+' - '+champion.getName()+' l\'archer')
       }else if(champion instanceof Mage && champion !== this){
         console.log(index+' - '+champion.getName()+' le mage')
-      }else if(champion instanceof Mage && champion !== this){
+      }else if(champion instanceof Mage && champion === this){
         console.log(index+' - '+ 'Vous-même')
       }
-    })
-
-    let input = prompt();
-
-    if(input){
+    });
+    
+    (async () => {
+      const response = await prompts({
+        type: 'number',
+        name: 'value',
+        message: 'Qui voulez-vous heal ?',
+        validate: value => value >= alive.length ? `Vous devez choisir un personnage disponible` : true
+      });
+      input = response.value
       target = alive[input]
       if (target.hpMax==target.hp) {
-        console.log('Vous êtes déjà full life !')
-      }else if(target.hpMax>target.hp){
+        console.log(target.getName()+' est déjà full life !')
+      }else{
         let difference = target.hpMax-target.hp
-        if (power<=difference) {
-          target.hp==target.hpMax
-          console.log('Vous êtes maintenant full life !')
+        if (power>=difference) {
+          target.hp=target.hpMax
+          console.log(target.getName()+' est maintenant full life !')
         } else {
           target.hp+=power
-          console.log('Vous avez été heal de '+power+' hp !')
+          console.log(target.getName()+' a été heal de '+power+' hp !')
+          console.log(target.getName()+' est maintenant à '+target.hp+' hp ❤')
         }
       }
-    }
+    
+    })();
+
+    // let input = prompt();
+
+
   }
 }
